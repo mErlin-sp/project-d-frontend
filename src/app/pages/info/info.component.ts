629e331a21a9b07f40ab67ea967440e22b945252
Oleksandr <merlin.ua.1@gmail.com>
2024-06-04 21:36:17 +0300

    Update info component

--- src/app/pages/info/info.component.ts
+++ src/app/pages/info/info.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {HomeButtonComponent} from "../../shared/home-button/home-button.component";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-info',
   standalone: true,
   imports: [
-    HomeButtonComponent
+    HomeButtonComponent,
+    NgOptimizedImage
   ],
   templateUrl: './info.component.html',
   styleUrl: './info.component.scss'
