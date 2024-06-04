77451102b8184b182bafd09db12eee02506ed47b
Oleksandr <merlin.ua.1@gmail.com>
2024-06-04 21:53:50 +0300

    Update api-docs component

--- src/app/pages/api-docs/api-docs.component.ts
+++ src/app/pages/api-docs/api-docs.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {HomeButtonComponent} from "../../shared/home-button/home-button.component";
 import {MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
 import {MatIcon} from "@angular/material/icon";
-import {MatIconButton} from "@angular/material/button";
+import {MatAnchor, MatIconButton} from "@angular/material/button";
 import {MatInput} from "@angular/material/input";
 import {ReactiveFormsModule} from "@angular/forms";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-api-docs',
@@ -17,11 +18,14 @@ import {ReactiveFormsModule} from "@angular/forms";
     MatInput,
     MatLabel,
     MatSuffix,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatAnchor
   ],
   templateUrl: './api-docs.component.html',
   styleUrl: './api-docs.component.scss'
 })
-export class ApiDocsComponent {
 
+
+export class ApiDocsComponent {
+  api_docs_url: string = `${environment.apiUrl}/docs`
 }
