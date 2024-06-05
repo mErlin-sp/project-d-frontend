d18d790d99921dc3664384b48844b1372521e08a
Oleksandr <merlin.ua.1@gmail.com>
2024-06-05 19:11:13 +0300

    Update export component

--- src/app/pages/export/export.component.ts
+++ src/app/pages/export/export.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {HomeButtonComponent} from "../../shared/home-button/home-button.component";
+import {MatAnchor} from "@angular/material/button";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-export',
   standalone: true,
   imports: [
-    HomeButtonComponent
+    HomeButtonComponent,
+    MatAnchor
   ],
   templateUrl: './export.component.html',
   styleUrl: './export.component.scss'
 })
-export class ExportComponent {
 
+export class ExportComponent {
+  service = environment.apiUrl
+  exportToCsv: string = `${this.service}/export/csv`
+  exportToXLSX: string = `${this.service}/export/xlsx`
+  exportToSqLite: string = `${this.service}/export/sqlite-db`
 }
