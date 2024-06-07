b950290bb35fb588f2216be9187fff7f50c7f2cb
Oleksandr <merlin.ua.1@gmail.com>
2024-06-07 06:43:46 +0300

    Add dist directory

--- docker.sh
+++ docker.sh
@@ -2,6 +2,8 @@
 docker build -t project-d-frontend .
 docker run -P -d \
     --name project-d-frontend \
+    --env SERVICE_ADDR=127.0.0.1 \
+    --env SERVICE_PORT=8000 \
     --network host \
     --restart always \
     project-d-frontend
