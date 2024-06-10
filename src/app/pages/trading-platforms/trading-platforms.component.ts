7beee79045fd178d6130842dc3de1d49f4809cf3
Oleksandr <merlin.ua.1@gmail.com>
2024-06-10 09:51:08 +0300

    Update trading-platforms component

--- src/app/pages/trading-platforms/trading-platforms.component.ts
+++ src/app/pages/trading-platforms/trading-platforms.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {HomeButtonComponent} from "../../shared/home-button/home-button.component";
 import {NgForOf, NgIf} from "@angular/common";
 import {DataService} from "../../data.service";
-import {catchError, interval, throwError} from "rxjs";
+import {catchError, interval, Subject, takeUntil, throwError} from "rxjs";
 
 @Component({
   selector: 'app-trading-platforms',
@@ -15,29 +15,37 @@ import {catchError, interval, throwError} from "rxjs";
   templateUrl: './trading-platforms.component.html',
   styleUrl: './trading-platforms.component.scss'
 })
-export class TradingPlatformsComponent implements OnInit {
+export class TradingPlatformsComponent implements OnInit, OnDestroy {
   platforms: string[] = [];
+
+  private destroy$ = new Subject<void>();
 
   constructor(private dataService: DataService) {
   }
 
   ngOnInit(): void {
     this.loadData();
-    interval(20000).subscribe(() => {
-      this.loadData();
-    });
+    interval(20000)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.loadData();
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   loadData(): void {
     this.dataService.getPlatforms().pipe(
       catchError(error => {
         console.error('Error loading platforms', error);
-
         return throwError(() => error);
       })
     ).subscribe((platforms: string[]) => {
       this.platforms = platforms;
-      console.debug('Platforms loaded', platforms);
+      console.log('Platforms loaded', platforms);
     });
   }
 }
